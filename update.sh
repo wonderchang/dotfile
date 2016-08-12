6ab3aede99036f8e424613dc3b52bd93eaa0ccc6
Wonder Chang <iwonder.tw@gmail.com>
2016-08-12 21:29:14 +0800

    Refine update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,22 @@
-echo "Updated .zshrc from $HOME/.zshrc"
-/bin/cp -f $HOME/.zshrc .
-echo "Updated .vimrc from $HOME/.vimrc"
-/bin/cp -f $HOME/.vimrc .
+update_all {
+  files=($@)
+  for fn in "${files[@]}"; do
+    if [ -f $HOME/$fn ]; then
+      echo "Updated from local config file $HOME/$fn"
+      /bin/cp -f $HOME/$fn .
+    else
+      echo "$HOME/$fn not exists, skip updating"
+    fi
+  done
+}
+
+dotfiles=('.zshrc' '.vimrc' '.tmux.conf' '.tmux.conf.local' '.gitconfig')
+OS=`uname -s`
+
+if [ "$OS" = "Darwin" ]; then
+  dotfiles+=(.bash_profile)
+  update_all "${dotfiles[@]}"
+elif [ "$OS" = "Linux" ]; then
+  dotfiles+=(.bashrc)
+  update_all "${dotfiles[@]}"
+fi
